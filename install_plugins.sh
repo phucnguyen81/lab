969c9bcc41b7f6901b8d526fa42122d4a2993a0a
Phuc K Nguyen <phucknguyen@gmail.com>
2022-06-26 14:11:11 +0700

    Let jenkins use docker

--- install_plugins.sh
+++ install_plugins.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+# Install jenkins
 sudo cp ./jenkins.yaml "${JENKINS_HOME}/jenkins.yaml"
 sudo wget -O "${JENKINS_HOME}/jenkins-plugin-manager.jar" https://github.com/jenkinsci/plugin-installation-manager-tool/releases/download/2.12.6/jenkins-plugin-manager-2.12.6.jar
 sudo java -jar "${JENKINS_HOME}/jenkins-plugin-manager.jar" --war /usr/share/java/jenkins.war --plugin-file ./plugins.txt --plugin-download-directory "${JENKINS_HOME}/plugins"
+
+# Let jenkins use docker
+sudo usermod -a -G docker jenkins
+
+# Restart jenkins to pickup the updates
 sudo service jenkins restart
