187803c63f1ce1ace02661f866b8d33aab6e9f23
Phuc K Nguyen <phucknguyen@gmail.com>
2022-06-30 14:24:23 +0700

    Install docker-compose together with docker

--- install_docker.sh
+++ install_docker.sh
@@ -5,7 +5,8 @@ sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
-sudo apt -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo apt -y install docker-ce docker-ce-cli containerd.io
+sudo apt -y install docker-compose-plugin docker-compose
 
 # Let vagrant user uses docker command
 sudo usermod -aG docker vagrant
