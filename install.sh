b99c079814f275da0c201b25d2c97f7c14805ab3
Phuc K Nguyen <phucknguyen@gmail.com>
2022-06-30 14:00:38 +0700

    Clean up install script

--- install.sh
+++ install.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-# Install pipenv for python development
-sudo apt update
-sudo apt --yes install software-properties-common
-sudo apt --yes install python3-pip
-sudo python3 -m pip install pipenv
-
 # Add python package repo
 sudo add-apt-repository ppa:deadsnakes/ppa --yes
 sudo apt update
+
+sudo apt --yes install software-properties-common python3-pip
+
+# Install pipenv for python development
+sudo python3 -m pip install pipenv
