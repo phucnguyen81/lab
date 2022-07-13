8a252a19785562a9eedeb204354759aeeff8e3d0
Phuc K Nguyen <phucknguyen@gmail.com>
2022-07-13 09:47:27 +0700

    Clean up

--- code/chapter10/src/main/java/com/apress/batch/chapter10/Chapter10Application.java
+++ code/chapter10/src/main/java/com/apress/batch/chapter10/Chapter10Application.java
@@ -1,24 +1,18 @@
 package com.apress.batch.chapter10;
 
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
 
 @SpringBootApplication
 public class Chapter10Application {
 
 	public static void main(String[] args) {
-		String[] realArgs = {"customerUpdateFile=file:///vagrant/code/Chapter10/src/main/resources/data/customer_update_shuffled.csv",
-		"transactionFile=file:///vagrant/code/Chapter10/src/main/resources/data/transactions.xml",
-		"outputDirectory=file:///vagrant/code/Chapter10/target/statements/"};
-
-		// String[] realArgs = {"customerUpdateFile=file:///Users/mminella/Documents/IntelliJWorkspace/def-guide-spring-batch/chapter10/src/main/resources/data/customer_update_shuffled.csv",
-		// "transactionFile=file:///Users/mminella/Documents/IntelliJWorkspace/def-guide-spring-batch/chapter10/src/main/resources/data/transactions.xml",
-		// "outputDirectory=file:///Users/mminella/Documents/IntelliJWorkspace/def-guide-spring-batch/chapter10/target/statements/"};
-//		String [] realArgs = {
-//				"customerUpdateFile=file:\\Users\\mminella\\IdeaProjects\\def-guide-spring-batch\\chapter10\\src\\main\\resources\\data\\customer_update_shuffled.csv",
-//				"transactionFile=file:\\Users\\mminella\\IdeaProjects\\def-guide-spring-batch\\chapter10\\src\\main\\resources\\data\\transactions.xml"
-//		};
+		String[] realArgs = {
+            "customerUpdateFile=file:///vagrant/code/Chapter10/src/main/resources/data/customer_update_shuffled.csv",
+		    "transactionFile=file:///vagrant/code/Chapter10/src/main/resources/data/transactions.xml",
+		    "outputDirectory=file:///vagrant/code/Chapter10/target/statements/"
+        };
 		SpringApplication.run(Chapter10Application.class, realArgs);
 	}
 }
